d5ba51616d59c0c3167db2d40f3831dca1fb4873
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-11-04 13:21:46 +0100

    Characters service

--- src/domain/services/characters.service.ts
+++ src/domain/services/characters.service.ts
@@ -1,56 +1,77 @@
 import { Injectable } from '@angular/core';
 import { CharactersRepoService } from 'src/infrastructure/repositories/characters-repo.service';
 import { ICharacter } from '../model/ICharacter';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: "root",
 })
 export class CharactersService {
-    private _characters = new BehaviorSubject<ICharacter[]>([]);
-    get characters() {
-        return this._characters;
+    private _characters: ICharacter[] = [];
+
+    private _totalCharacters = 0; // Updated every time characters are obtained.
+    get totalCharacters() {
+        return this._totalCharacters;
     }
-    private _filteredCharacters = new BehaviorSubject<ICharacter[]>([]);
-    get filteredCharacters() {
-        return this._filteredCharacters;
+
+    private _nextPage: string | null = "1"; // Next page number
+    get nextPage() {
+        return this._nextPage;
     }
-    private nextPage: string | null = "1";
-    private loading = false;
+
+    private _characters$ = new Subject<ICharacter[]>(); // Total characters obtained
+    get characters$() {
+        return this._characters$;
+    }
+
+    private _filteredCharacters$ = new Subject<ICharacter[]>(); // Filtered characters
+    get filteredCharacters$() {
+        return this._filteredCharacters$;
+    }
+
+    private _loading = false; // Reading from repository
+    get loading() {
+        return this._loading;
+    }
 
     constructor(private charactersRepo: CharactersRepoService) {}
 
-    ngOnInit(): void {
-        this.getCharacters();
-    }
-
-    getNextPage(): string | null {
-        return this.nextPage
-    }
-
-    getCharacters(): void {
+    /**
+     * Gets the next page of characters from the repository
+     *
+     */
+    getCharacters() {
         if (this.nextPage == null || this.loading) return;
 
-        this.loading = true;
+        this._loading = true;
 
         this.charactersRepo.getCharacters({ page: this.nextPage }).subscribe({
             next: resp => {
-                const val = this.characters.getValue();
                 const next = resp.data[0].info.next;
 
-                this.nextPage = next == null ? null : this.getPage(next);
-                console.log(resp.data[0].info.next, this.nextPage);
-                val.push(...resp.data[0].results);
-                this._characters.next(val);
+                this._totalCharacters = resp.data[0].info.count;
+                this._nextPage = next == null ? null : this.getPage(next); // Gets new data page from the next url
+                this._characters.push(...resp.data[0].results); // Add new characters to those already obtained.
+                this._characters$.next(this._characters); // The Observable (Subject) 'characters$' emits the new values
             },
             error: err => console.log(err),
-            complete: () => (this.loading = false),
+            complete: () => (this._loading = false),
         });
     }
 
+    /**
+     * Enter new values in the Observable (Subject) 'filteredCharacters$'
+     *
+     * @param characters Array of characters filtered by name from the header component
+     */
     setFilteredCharacters(characters: ICharacter[]): void {
-        this._filteredCharacters.next(characters);
+        this._filteredCharacters$.next(characters);
     }
 
+    /**
+     *
+     * @param url URL string from which to get the page query parameter
+     * @returns The page number as a string or null if not found
+     */
     private getPage = (url: string) => url.split("?page=").pop() ?? null;
 }
