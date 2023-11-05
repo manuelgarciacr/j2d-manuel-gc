810143b74c080e2cd2dfb4aec03da67c442456e2
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-11-05 18:00:52 +0100

    App component

--- src/infrastructure/components/card/card.component.ts
+++ src/infrastructure/components/card/card.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-// import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { ICharacter } from 'src/domain/model/ICharacter';
 
@@ -13,4 +12,15 @@ import { ICharacter } from 'src/domain/model/ICharacter';
 export class CardComponent {
     @Input() character!: ICharacter;
     protected clicked = 0;
+
+    constructor(private host: ElementRef){}
+
+    onClick() {
+
+        setTimeout(() => {
+            // It needs a waiting time to work correctly
+            this.clicked =
+                this.clicked == this.character.id ? 0 : this.character.id;
+        }, 100)
+    }
 }
